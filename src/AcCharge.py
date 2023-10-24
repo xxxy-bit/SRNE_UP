b1c851b1ffaf81395f7e933c9f02db7a421769cf
hxyxz <785815707@qq.com>
2023-10-24 10:11:58 +0800

    打包exe,充电器版本号 v0.0.5

--- src/AcCharge.py
+++ src/AcCharge.py
@@ -18,7 +18,7 @@ class AcLayout(QtWidgets.QMainWindow, ac_layout):
         self.setupUi(self)
         self.ac_layout_init()
         self.i18n_init()
-        self.setWindowTitle(f'{self.windowTitle()} v0.0.4')
+        self.setWindowTitle(f'{self.windowTitle()} v0.0.5')
     
     # 加载国际化i18n
     def i18n_init(self):
